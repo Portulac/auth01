5277570c050d07d02e1ea8c304f97da57a0d795c
Portulac <molokolbasa@gmail.com>
2021-05-14 13:28:09 +0300

    Add checklistcontroller, do correct redirect after authorization (/)

---
+++ src/Controller/ChecklistController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ChecklistController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    
+    public function homepage()
+    {
+        return $this->render('checklist/index.html.twig', [
+            'controller_name' => 'ChecklistController',
+        ]);
+    }
+
+    /**
+     * @Route("/checklist", name="checklist")
+     */
+    
+    public function index(): Response
+    {
+        return $this->render('checklist/index.html.twig', [
+            'controller_name' => 'ChecklistController',
+        ]);
+    }
+}
